c30231595eddfb90e294be63c58a0684b3e91341
Anna Brio <73587031+Anna-Brio@users.noreply.github.com>
2021-01-30 20:03:08 +0200

    Add files via upload

---
+++ telebot_config.py
@@ -0,0 +1,11 @@
+TOKEN = '1495488888:AAEm7IKMvdeLCiMgWT9jNs87b3Lqbby9Tpw'
+keys = {
+    'euro': 'EUR',
+    'pound': 'GBP',
+    'dollar': 'USD',
+    'australian dollar': 'AUD',
+    'new zealand dollar': 'NZD',
+    'zloty': 'PLN',
+    'ruble': 'RUB',
+    'shekel':'ILS'
+}
